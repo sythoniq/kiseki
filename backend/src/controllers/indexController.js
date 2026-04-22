d7e5f26c9159f1f313da448921c9058019eedb4b
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-04-22 17:27:20 +0300

    fix: used the same variable name as res thus no response was sent after user creation

--- backend/src/controllers/indexController.js
+++ backend/src/controllers/indexController.js
@@ -1,14 +1,28 @@
+const bcrypt = require('bcryptjs')
 const prisma = require('../configs/prisma.js')
 
-async function handleLogin(req, res, next) {
+const checkUsername = require('../configs/helpers.js').checkUsername;
+
+async function handleRegister(req, res, next) {
   try {
-    console.log('hi')
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 10)
+    const result = await checkUsername(username);  
+    if (result) {
+      const user = await prisma.user.create({
+        data: {
+          name: username,
+          hash
+        }
+      }) 
+      return res.json({success: true, user})
+    }
   } catch(err) {
-    console.error(err)
+    return res.json({success: false, msg: "Failed to create user", err})
   }
 }
 
-async function handleRegister(req, res, next) {
+async function handleLogin(req, res, next) {
   try {
     console.log('hi')
   } catch(err) {
