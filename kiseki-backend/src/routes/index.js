11931c5bd50c3c4fa95f709d5d643840f088c3e0
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-04-23 13:32:45 +0300

    feat: added cors and changed the db schema

--- kiseki-backend/src/routes/index.js
+++ kiseki-backend/src/routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require("express")
 const index = Router()
 
 const controller = require("../controllers/indexController.js")
+const auth = require("../configs/helpers.js").validateUser
 
 index.post("/register", controller.handleRegister)
 index.post("/login", controller.handleLogin)
+index.post("/auth", auth, (req, res) => {res.json({success: true, msg: "User authrorized"})})
 
 module.exports = index
