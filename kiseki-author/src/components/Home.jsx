51c59e8964c0b70896c692748ea479c5326a1fd3
sythe <188770367+sythoniq@users.noreply.github.com>
2026-05-02 22:49:51 +0300

    bug/fail: idk what to tag this but ive failed to do anything as I feel the need to go back and go through react state to properly understand it to prevent unnecessary rerenders -_-

--- kiseki-author/src/components/Home.jsx
+++ kiseki-author/src/components/Home.jsx
@@ -5,19 +5,36 @@ import Post from './Post.jsx'
 
 export default function Home() {
   const loadedPosts = useLoaderData()
-  console.log(loadedPosts);
+  const [posts, setPosts] = useState()
+  
+  if (loadedPosts !== null || loadedPost !== undefined) {
+    console.log("passed")
+    setPosts(loadedPosts) 
+  }
+  
+  if (!posts) {
+    return (
+      <main className="posts-page">
+        <p>No posts currently..</p>
+      </main>
+    )
+  } 
 
-  const publishedPosts = loadedPosts.filter((post) => post.published === true)
-  const unpublishedPosts = loadedPosts.filter((post) => post.published === false)
+  const publishedPosts = posts.filter((post) => post.published === true)
+  const unpublishedPosts = posts.filter((post) => post.published === false)
 
   return (
     <main className="posts-page">
+      <div className="create-post">
+      </div>
       <div className="published-posts">
+        <h4>Published Posts</h4>
         {publishedPosts && publishedPosts.map((post) => {
           <Post title={post.title} uploadedAt={post.uploadedAt} published={post.published} />
         })}
       </div>
       <div className="unpublished-posts">
+        <h4>Unpublished Posts</h4>
         {unpublishedPosts && unpublishedPosts.map((post) => {
           <Post title={post.title} uploadedAt={post.uploadedAt} published={post.published} />
         })}
