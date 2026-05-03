df0c8b0133113f81c7077e62a793d52a5021da97
sythe <188770367+sythoniq@users.noreply.github.com>
2026-05-03 19:51:12 +0300

    feat: added some routes for other author features

--- kiseki-backend/src/controllers/postController.js
+++ kiseki-backend/src/controllers/postController.js
@@ -62,10 +62,123 @@ async function postComment(req, res, next) {
   } 
 }
 
+async function deletePost(req, res, next) {
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: Number(req.params.postId)
+      }
+    })
+    if (post) {
+      await prisma.post.delete({
+        where: {
+          id: Number(req.params.postid)
+        }
+      })
+      return res.json({success: true, msg: "Post deleted successfully"})
+    } else {
+      throw new Error("Post doesnt exist")
+    }
+  } catch(err) {
+    return res.json({success: false, msg: err});
+  }
+}
+
+async function deletePostComment(req, res, next) {
+  try {
+    const comment = await prisma.comment.findUnique({
+      where: {
+        id: Number(req.params.commentId)
+      }
+    });
+    if (comment) {
+      await prisma.comment.delete({
+        where: {
+          id: Number(req.params.commentId)
+        }
+      }) 
+      return res.json({success: true, msg: "Comment deleted"})
+    } else {
+      throw new Error("Comment not found")
+    }
+  } catch (err) {
+    return res.json({success: false, msg: err}) 
+  }
+}
+
+async function updatePost(req, res, next) {
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: Number(req.params.postId)
+      }
+    })
+    if (post) {
+      await prisma.post.update({
+        where: { id: Number(req.params.postId ) },
+        data: { title: req.body.title, content: req.body.content}
+      })
+      return res.json({success: true, msg: "Post updated"})
+    } else {
+      throw new Error("Post not found")
+    }
+  } catch(err) {
+    console.log(err);
+    return res.json({success: false, msg: err}) 
+  }
+}
+
+async function publishPost(req, res, next) {
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: Number(req.params.postId)
+      }
+    })
+    if (post) {
+      await prisma.post.update({
+        where: { id: Number(req.params.postId) },
+        data: { published: true }
+      })
+      return res.json({success: true, msg: "Post published"})
+    } else {
+      throw new Error("Post not found")
+    }
+  } catch(err) {
+    return res.json({success: false, msg: err})
+  }
+}
+
+async function unpublishPost(req, res, next) {
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: Number(req.params.postId)
+      }
+    }) 
+    if (post) {
+      await prisma.post.update({
+        where: { id: Number(req.params.postId) },
+        data: { published: false }
+      }) 
+      return res.json({success: true, msg: "Unpublished post"})
+    } else {
+      throw new Error("Post not found")
+    }
+  } catch(err) {
+    return res.json({success: false, msg: err})
+  }
+}
+
 module.exports = {
   getPosts,
   getPost,
   getPostComments,
   uploadPost,
-  postComment
+  updatePost,
+  postComment,
+  deletePost,
+  deletePostComment,
+  publishPost,
+  unpublishPost
 }
--- kiseki-backend/src/routes/posts.js
+++ kiseki-backend/src/routes/posts.js
@@ -8,7 +8,14 @@ posts.get("/", controller.getPosts)
 posts.get("/:postId", controller.getPost)
 posts.get("/:postId/comments", controller.getPostComments)
 
+
 posts.post("/upload", auth.validateUser, auth.validateAuthor, controller.uploadPost)
+posts.post("/:postId/update", auth.validateUser, auth.validateAuthor, controller.updatePost)
 posts.post("/:postId/comment", auth.validateUser, controller.postComment)
+posts.post("/:postId/delete", auth.validateUser, auth.validateAuthor, controller.deletePost)
+posts.post("/:postId/comment/:commentId", auth.validateUser, auth.validateAuthor, controller.deletePostComment)
+posts.post("/:postId/publish", auth.validateUser, auth.validateAuthor, controller.publishPost)
+posts.post("/:postId/unpublish", auth.validateUser, auth.validateAuthor, controller.unpublishPost);
+
 
 module.exports = posts
