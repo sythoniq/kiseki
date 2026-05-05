0d89f4ae915628a98b588c0d4021acee60a80a98
sythe <188770367+sythoniq@users.noreply.github.com>
2026-05-05 16:29:16 +0300

    feat: the end for now

--- kiseki-backend/src/routes/index.js
+++ kiseki-backend/src/routes/index.js
@@ -4,7 +4,6 @@ const index = Router()
 const controller = require("../controllers/indexController.js")
 const auth = require("../configs/helpers.js")
 
-index.post("/register", controller.handleRegister)
 index.post("/login", controller.handleLogin)
 index.post("/auth", auth.validateUser, (req, res) => {
   const user = req.user;
