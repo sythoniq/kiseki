9525cd5c913a74abb4fc4a6214c676f891213a97
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-08-23 22:06:36 +0300

    feat: working on the content part

--- client/src/components/content/Content.jsx
+++ client/src/components/content/Content.jsx
@@ -1,10 +1,32 @@
 import styles from './content.module.css'
 import { useNavigate, Link } from 'react-router'
+import useGetPosts from '../../hooks/useGetPosts.js'
 
 export default function Content() {
+	const [ posts, postCategories, isLoading, isError ] = useGetPosts()
+	if (isLoading) {
+		return (
+			<span>Loading..</span>
+		)
+	}
+	
+	if (isError) {
+		return (
+			<span>Error </span>
+		)
+	}
+
+	const postsList = posts.map(post => 
+		<section key={post.post_id} className={styles.postCard}>
+			<Link to={`posts/${post.post_id}`}><h2>{post.post_title}</h2></Link>
+			<div className={styles.postDate}>{new Date(post.uploadedAt).toLocaleDateString(undefined, {year: "numeric", month: "long", day: "numeric"})}</div>
+			<div className={styles.categoryTag}>{post.post_category}</div>
+		</section>
+	)
+
 	return (
 		<main className={styles.mainContent}>
-
+			{postsList}	
 		</main>
 	)
 }
