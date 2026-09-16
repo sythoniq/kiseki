e68ffdae86a35a23e352c15123c57a2edabd155a
sythe <188770367+sythoniq@users.noreply.github.com>
2026-09-16 11:19:52 +0300

    feat: added back btn and other small features

--- client/src/components/author/content/Content.jsx
+++ client/src/components/author/content/Content.jsx
@@ -1,13 +1,17 @@
 import styles from "./content.module.css"
 import toast from 'react-hot-toast'
-import { useParams } from 'react-router'
+import { useParams, Link } from 'react-router'
+import { useState } from 'react'
 
 import useGetPost from '../../../hooks/useGetPost.js'
 
 export default function PostPage() {
-	const postId = useParams().postId
+	const API = import.meta.env.VITE_BASE_API
+	const TOKEN = localStorage.getItem("jwt-token")
 
+	const postId = useParams().postId
 	const [post, loading, error] = useGetPost(postId)
+	const [postState, setPostState] = useState()
 
 	if (loading) {
 		return (
@@ -24,17 +28,53 @@ export default function PostPage() {
 	}
 
 	async function handlePublish() {
+		try {
+			const res = await fetch(`${API}/posts/${postId}/publish`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+					"Authorization": TOKEN
+				}
+			})
+			const data = await res.json()
+			if (!data.success) {
+				toast.error(data.message)
+				return;
+			}
 
+			toast.success("Published")
+			setPostState(true)
+		} catch(e) {
+			return toast.error("Something went wrong")
+		}
 	}
 	
 	async function handleUnpublish() {
+		try {
+			const res = await fetch(`${API}/posts/${postId}/unpublish`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+					"Authorization": TOKEN
+				}
+			})
+			const data = await res.json()
+			if (!data.success) {
+				toast.error(data.message)
+				return;
+			}
 
+			toast.success("Unpublished!")
+			setPostState(false)
+		} catch(e) { 
+			return toast.error("Something went wrong")
+		}
 	}
-
 	return (
 		<section className={styles.content}>
 			<main className={styles.post}>
 				<div className={styles.postDetails}>
+					<Link to="/author"><button className={styles.backBtn}>Back</button></Link>
 					<div>
 						<span className={styles.postDate}>
 							{new Date(post.uploadedAt).toLocaleDateString(undefined,
@@ -43,7 +83,7 @@ export default function PostPage() {
 						</span>	
 						<span className={styles.postCateg}>{post.post_category}</span>
 						<div className={styles.postState}>
-							{post.published ? (
+							{post.published || postState ? (
 								<button className={styles.unpubBtn} onClick={handleUnpublish}>Unpublish</button>
 							) : (
 									<button className={styles.pubBtn} onClick={handlePublish}>Publish</button>
